92c91791925a69a05de34849abe13eda13fd4599
jv88899 <jv88899@gmail.com>
2021-07-27 18:31:55 -0500

    created useRoom hook

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Chat.css";
+import useRoom from "../hooks/useRoom";
+import { useParams } from "react-router-dom";
 
-export default function Chat() {
+export default function Chat({ user, page }) {
+  const { roomId } = useParams();
+  const room = useRoom(roomId, user.uid);
+
   return <div className="chat">Chat</div>;
 }
