d7e5e5a2e2522bde9dd46990d83cd9be7fae6b43
jv88899 <jv88899@gmail.com>
2021-07-27 16:15:24 -0500

    removed title prop from SidebarListItem in SidebarList

--- src/components/SidebarList.js
+++ src/components/SidebarList.js
@@ -30,7 +30,7 @@ export default function SidebarList({ title, data }) {
     <div className="sidebar__chat--container">
       <h2>{title}</h2>
       {data.map((item) => (
-        <SidebarListItem key={item.id} item={item} title={title} />
+        <SidebarListItem key={item.id} item={item} />
       ))}
     </div>
   );
