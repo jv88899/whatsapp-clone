d6e0173724f5391945dba246a9a600a701ae975f
jv88899 <jv88899@gmail.com>
2021-07-29 13:25:42 -0500

    set up functionality to load audio files into AudioPlayer

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -17,6 +17,75 @@ export default function AudioPlayer({
   const [sliderValue, setSliderValue] = React.useState(0);
   const [duration, setDuration] = React.useState("");
 
+  const totalDuration = React.useRef("");
+  const audio = React.useRef(new Audio(audioUrl));
+  const interval = React.useRef();
+  const isUploading = React.useRef(audioUrl === "uploading");
+
+  React.useEffect(() => {
+    if (isUploading.current && audioUrl !== "uploading") {
+      audio.current = new Audio(audioUrl);
+      audio.current.load();
+      setLoaded(true);
+    } else if (isUploading.current === false) {
+      setLoaded(true);
+    }
+  }, [audioUrl]);
+
+  function getAudioDuration(media) {
+    return new Promise((resolve) => {
+      media.onloadedmetadata = () => {
+        media.currentTime = Number.MAX_SAFE_INTEGER;
+        media.ontimeupdate = () => {
+          media.ontimeupdate = () => {};
+          media.currentTime = 0.1;
+          resolve(media.duration);
+        };
+      };
+    });
+  }
+
+  React.useEffect(() => {
+    if (isLoaded) {
+      getAudioDuration(audio.current).then(() => {
+        setMetadataLoaded(true);
+      });
+    }
+  }, [isLoaded]);
+
+  React.useEffect(() => {
+    if (isMetadataLoaded) {
+      audio.current.addEventListener("canplaythrough", () => {
+        if (!totalDuration.current) {
+          setMediaLoaded(true);
+          const time = formatTime(audio.current.duration);
+          totalDuration.current = time;
+          setDuration(totalDuration.current);
+        }
+      });
+      audio.current.addEventListener("ended", () => {
+        clearInterval(interval.current);
+        setDuration(totalDuration.current);
+        setSliderValue(0);
+        setPlaying(false);
+      });
+    }
+  }, [isMetadataLoaded]);
+
+  function formatTime(time) {
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time - minutes * 60);
+
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+
+    return `${minutes}:${seconds}`;
+  }
+
   return (
     <>
       <div className={`audioplayer ${sender ? "" : "audioplayer__alt"}`}>
